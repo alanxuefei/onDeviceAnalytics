2be67835e4a756c68c36356d4bcf44b4042ecc36
NewUser <newuser@keekiat-mbp.local>
2015-10-20 14:58:30 +0800

    Initial Commit

---
+++ onDeviceAnalytics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  onDeviceAnalytics
+//
+//  Created by NewUser on 20/10/15.
+//  Copyright © 2015 mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ onDeviceAnalytics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  onDeviceAnalytics
+//
+//  Created by NewUser on 20/10/15.
+//  Copyright © 2015 mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
